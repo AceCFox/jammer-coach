658dec0439f0f9d6d0a755fda293d5cd7bd754a0
Ace Fox <afox09@gmail.com>
2020-08-03 17:50:08 -0500

    contained component logic for AddSkill

--- src/components/AddSkill/AddSkill.js
+++ src/components/AddSkill/AddSkill.js
@@ -29,8 +29,55 @@ const styles = theme => ({
 
 class AddSkill extends Component {
   state = {
-    newUrl:'',
-    urlArray: []};
+    title:'',
+    author:'',
+    url:'',
+    description:'',
+    thisCategory: [],
+    allCategory:['hockey stop', 'plow', 'edges', 'toe stop', 'agility', 'crossover'],
+    category: ''
+    };
+
+    componentDidMount(){
+        //dispatch saga to fetch categories
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+      };
+
+    handleAdd = (event)=>{
+        let array = this.state.thisCategory;
+        array.push(this.state.category);
+        // console.log(array);
+        this.setState({
+            ...this.state,
+            thisCategory: array,
+            category:'',
+        })//end setState
+    }//end handleAdd
+
+    handleRemove = (id) => (event) =>{
+        console.log('remove', id);
+        let array = this.state.thisCategory;
+        array.splice(id, 1);
+        this.setState({
+            ...this.state,
+            thisCategory: array
+        })//end setStae
+    }//end handleRemove
+
+    handleSubmit = () =>{
+        const submitObject = {
+            title: this.state.title,
+            author: this.state.author,
+            url: this.state.url,
+            categories: this.state.thisCategory,
+            description: this.state.description,
+        }
+        console.log('object to post:', submitObject);
+        //dispatch a saga to post submitObject
+    }
 
   render() {
     const {classes} = this.props;
@@ -39,49 +86,68 @@ class AddSkill extends Component {
           <Paper className = {classes.paper}>
                 <h2>Add A Skill</h2>
                 {/* Eventually, this next bit should be conditional upon user != coach */}
-                <p><i>Note: these viedeos are accessable by all users</i></p>
+                <p><i>Note: skill videos are accessable by all users</i></p>
                 {/* A link to the add self footage page will appear here if the user is not a coach */}
                 <TextField
                 id="title-in"
                 label="Title"
+                name='title'
+                value = {this.state.title}
+                onChange = {this.handleChange}
                 className={classes.textField}
                 margin="normal"/>
                 {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
                 <TextField
                 id="author-in"
-                label="Author"
+                label="Creator"
+                name='author'
+                value = {this.state.author}
+                onChange = {this.handleChange}
                 className={classes.textField}
                 margin="normal"/>
                 <TextField
                 id="url-in"
                 label="Youtube Url"
+                name='url'
+                value = {this.state.url}
+                onChange = {this.handleChange}
                 className={classes.longField}
                 margin="normal"/>
                 <FormControl className={classes.formControl}>
                     <InputLabel htmlFor="age-simple">Category</InputLabel>
                     {/* TO DO: CONTROLL ALL THESE INPUTS */}
                     <Select
+                        value = {this.state.category}
+                        onChange={this.handleChange}
                         inputProps={{
-                        name: 'category',
-                        id: 'category-in',
+                            name: 'category',
+                            id: 'category-in',  
                         }}>
                             <MenuItem value="">
-                            <em>None</em>
+                            <em></em>
                             </MenuItem>
-                            <MenuItem value='10'>Ten</MenuItem>
-                            <MenuItem value='20'>Twenty</MenuItem>
-                            <MenuItem value='30'>Thirty</MenuItem>
+                            {/* TO DO: CHANGE this.state.allcategory to the category reducer props */}
+                            {this.state.allCategory.map((category, index) =>(
+                                <MenuItem value={category} key ={index}>{category}</MenuItem>
+                            ))}
                         </Select>
-                        <Button color = "primary">Add category</Button>
+                        <Button color = "primary" onClick = {this.handleAdd}>Add category</Button>
                     </FormControl>
                     <ul className = {classes.categoryList} >
                         <label>Selected Categories</label>
-                        <li>sample category <Button color="secondary">Remove</Button></li>
-                        <li>another category <Button color="secondary">Remove</Button></li>
+                        {this.state.thisCategory.map((category, index)=>(
+                           <li key = {index}>{category} 
+                           <Button value = {index} color="secondary" onClick = {this.handleRemove(index)}>
+                               Remove
+                            </Button></li> 
+                        ))}
                     </ul>
                 <TextField
                 id="standard-multiline-flexible"
                 label="Description"
+                name ='description'
+                value = {this.state.description}
+                onChange = {this.handleChange}
                 multiline
                 rowsMax="4"
                 className={classes.longField}
@@ -89,7 +155,9 @@ class AddSkill extends Component {
                 />
                 <br/>
                 <br/>
-            <Button variant ='contained' color = 'primary' size = "large">Submit</Button>
+            <Button variant ='contained' color = 'primary' size = "large" onClick = {this.handleSubmit}>
+                Submit
+            </Button>
           </Paper> 
       </div>
     );
