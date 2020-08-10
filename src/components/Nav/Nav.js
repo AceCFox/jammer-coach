ccb39fb93e4241caf669634ec058b041774611ad
Ace Fox <afox09@gmail.com>
2020-08-10 15:01:23 -0500

    hamburger menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,73 +1,164 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
-import { Toolbar, AppBar, } from '@material-ui/core';
+import { Toolbar, AppBar, List, ListItem, Drawer, Divider, ListItemIcon, ListItemText, IconButton} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
-const Nav = (props) => (
-  <div className="nav">
-    <AppBar position = 'static'>
-      <Toolbar>
-        <Link className = 'nav-link' to="/home">
-           <b>Jammer Coach</b> 
-        </Link>
-        <img className = "icon" src = {require ('./favicon.ico') }alt = 'roller skate icon'/>
-        <div className="nav-right">
-          <Link className="nav-link" to="/home">
-            {/* Show this link if they are logged in or not,
-            but call this link 'Home' if they are logged in,
-            and call this link 'Login / Register' if they are not */}
-            {props.user.id ? 'Home' : 'Login / Register'}
-          </Link>
-          {/* Show the link to the info page and the logout button if the user is logged in */}
-          {props.user.id && (
-            <>
-              <Link className="nav-link" to="/info">
-                Info Page
-              </Link>
-              <Link className="nav-link" to="/AddSkill">
-                Add a Skill
-              </Link>
-              <Link className="nav-link" to="/curriculum">
-                My Curriculum
-              </Link>
-            </>    
-          )}
-            {props.user.is_coach && (
-           <>
-             <Link className="nav-link" to="/AssignList">
-             Assign Skills
-           </Link>
-           <Link className="nav-link" to="/manage">
-             Manage Skaters
-           </Link>
-           <Link className="nav-link" to="/EditSkill">
-             Edit Skills
-           </Link>
-         </>
-          )}
-          {/* Always show this link since the about page is not protected */}
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </div>
-        <div className = 'nav-end'>
-        <LogOutButton className="nav-link"/>
-        </div>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+const drawerWidth = 240;
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  logout: {
+    marginLeft: '85%'
+  },
+  icon:{
+    height: 50,
+    width: 'auto',
+  },
+});
+
+
+
+class Nav extends Component{
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
+  render() {
+    const {classes} = this.props;
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <List>
+          <ListItem>
+            <ListItemIcon>
+            <img className = "icon" src = {require ('./favicon.ico') }alt = 'roller skate icon'/>
+            </ListItemIcon>
+            <ListItemText primary ='JammerCoach'/>
+          </ListItem>
+        </List>
+        
+        
+        <Divider />
+        {this.props.reduxState.user.is_coach && (
+          <>
+          <List>
+            <ListItem button component = {Link} to ="/AssignList" >
+              <ListItemText primary ='Assign Skills'/>
+            </ListItem>     
+            <ListItem button component = {Link} to = '/manage'>
+              <ListItemText primary ='Manage Skaters'/>
+            </ListItem>     
+            <ListItem button component = {Link} to = '/EditSkills'>
+              <ListItemText primary ='Edit Skills'/>
+            </ListItem>   
+          </List>
+          <Divider />
+        </>
+        )}
+         {this.props.reduxState.user.id && (
+          <>
+          <List>
+            <ListItem button component = {Link} to ='/home'>
+              <ListItemText primary ='Profile'/>
+            </ListItem>
+            <ListItem button component = {Link} to = '/curriculum'>
+              <ListItemText primary ='My Curriculum'/>
+            </ListItem>
+            <ListItem button component = {Link} to = '/AddSkill'>
+              <ListItemText primary ='Add A Skill'/>
+            </ListItem>
+          </List>
+          <Divider />
+        </>
+        )}
+        <List>
+          <ListItem >
+            <LogOutButton/>
+          </ListItem>
+        </List>
+      </div>
+    )
+    return (
+    <div className="nav">
+      <AppBar position = 'static'>
+        <Toolbar>
+          <IconButton
+              color="inherit"
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+          </IconButton>
+          <h3>Jammer Coach</h3>
+          {'\u00A0'}{'\u00A0'}
+          <img className = {classes.icon} src = {require ('./favicon.ico') }alt = 'roller skate icon'/>
+          
+          {/* <LogOutButton /> */}
+         
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer}>
+            <Drawer
+              container={this.props.container}
+              anchor="left"
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+        </nav>
+    </div>
+  );
+  }
+}
+
+Nav.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
 // if they are logged in, we show them a few more links 
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({ user }) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
+const mapStateToProps = reduxState => ({
+  reduxState,
 });
-
-export default connect(mapStateToProps)(Nav);
+export default withStyles(styles)(connect(mapStateToProps)(Nav));
