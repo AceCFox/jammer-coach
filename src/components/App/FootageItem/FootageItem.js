9aef0ed70a23d4775745cc1e58bc6b1b4ba43c33
Ace Fox <afox09@gmail.com>
2020-08-12 14:11:33 -0500

    footage component framework

---
+++ src/components/App/FootageItem/FootageItem.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+import {Button, Paper,  Grid} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import ReactPlayer from 'react-player/lazy'
+
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justify:'center,'
+    },
+    paper: {
+        width: '100%',
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        justify: 'center'
+    },
+    video :{
+        maxWidth: '100%',
+    },
+  });
+
+class FootageItem extends Component {
+  state = {
+    open: false,
+    };
+
+    componentDidMount(){
+       
+    }
+    
+    handleOpen = () =>{
+        this.setState({
+            open: true,
+        })
+    }
+
+  render() {
+    const {classes} = this.props;
+    return (
+      <div className={classes.root}>
+         <Grid container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing = {4}>
+             <Grid item xs = {12} sm = {11} md ={9} lg = {8} xl = {7}>         
+                <Paper className = {classes.paper}>
+                    <Grid container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        spacing = {0}>
+                        <Grid item xs = {12} md = {8}>
+                            <Grid container
+                                direction="column"
+                                justify="center"
+                                alignItems="center">
+                                <ReactPlayer url= {this.props.footage.url} 
+                                    controls = {true} 
+                                    alt = {this.props.footage.added_by}
+                                    className = {classes.video}/> 
+                            </Grid>
+                        </Grid>
+                    <Grid item xs = {12} md = {3}> 
+                        <h2>{this.props.footage.added_by}'s Footage</h2>
+                        <p>{this.props.footage.notes}</p>
+                        <Button variant = 'outlined' color = 'secondary'
+                        onClick = {this.handleOpen}>
+                            Delete this footage
+                        </Button>
+                    </Grid>
+                  </Grid>
+                </Paper>
+             </Grid>  
+          </Grid>
+      </div>
+    );
+  }
+}
+
+FootageItem.propTypes = {
+    classes: PropTypes.object.isRequired
+  };
+  
+  const mapStateToProps = reduxState => ({
+    reduxState,
+  });
+  
+  export default withStyles(styles)(connect(mapStateToProps)(FootageItem));
