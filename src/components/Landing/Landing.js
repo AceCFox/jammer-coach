5a375eb7c6d022752099a18046477ff3779731ec
Ace Fox <afox09@gmail.com>
2020-08-14 11:29:15 -0500

    enhanced styling landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -14,14 +14,14 @@ const styles = theme => ({
       justify: 'center',
       backgroundColor: 'black',
       backgroundImage: 'url(https://images.squarespace-cdn.com/content/v1/5b7e168725bf02338601dd50/1534993658578-3NPEWQJERIOO4IX3B82Q/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/derby-13.jpg)',
-      minHeight: '800px',
+      minHeight: '840px',
       backgroundPosition:'center bottom',
       backgroundRepeat: 'no-repeat',
       backgrountAttachment: 'fixed',
   },
   paper: {
       width: '100%',
-      padding: theme.spacing(2),
+      padding: theme.spacing(1),
       textAlign: 'center',
       color: theme.palette.text.secondary,
       justify: 'center',
@@ -34,7 +34,6 @@ class Landing extends Component {
     const {classes} = this.props;
     return (
       <div className = {classes.root}>
-         {/* <br/> */}
         <Grid container direction="row"
             justify="center"
             alignItems="center">
@@ -59,9 +58,15 @@ class Landing extends Component {
                             </Button>
                         </Grid>
                         <Grid item xs = {12} sm = {6} md ={3}>
-                            <Button variant = 'contained'
+                            <Button variant = 'contained' color = 'secondary'
                             component = {Link} to = '/AssignList'>
                                 Assign Skills
+                            </Button>
+                        </Grid>
+                        <Grid item xs = {12} sm = {12} md ={3}>
+                            <Button variant = 'contained'
+                            component = {Link} to = '/AddSkill'>
+                                Add Skills
                             </Button>
                         </Grid>
                     </>
@@ -82,9 +87,13 @@ class Landing extends Component {
                         </Grid>
                     </>
                     }
-                <br/>
-                <br/>
+                   
                 </Grid>
+                <Grid container direction = 'row' justify = 'center' alignItems = 'center' spacing ={0}> 
+                    <h2>&#8598; </h2>
+                    {'\u00A0'}{'\u00A0'}
+                    <i>More features can be found in the list menu</i>
+                 </Grid>
             </Paper>
           </Grid>
         </Grid>
