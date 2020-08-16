debebcf44396d207a53cb797f673ca7a5df4c308
Ace Fox <afox09@gmail.com>
2020-08-16 15:23:07 -0500

    deploying

---
+++ build/precache-manifest.62d073d67426e5923b5e468ca682b75b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4421715b51feed73aea4ad0b211f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1c342c3cc788355e7ad",
+    "url": "/static/css/main.97bf76a2.chunk.css"
+  },
+  {
+    "revision": "93b0aeca254a8ab57701",
+    "url": "/static/js/14.571988f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/14.571988f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c342c3cc788355e7ad",
+    "url": "/static/js/main.093bf3d2.chunk.js"
+  },
+  {
+    "revision": "b2492a1fdbc41f5c2d91",
+    "url": "/static/js/reactPlayerDailyMotion.bbf539c5.chunk.js"
+  },
+  {
+    "revision": "3aa8e1c2d6cd5c7bdb2f",
+    "url": "/static/js/reactPlayerFacebook.865854d4.chunk.js"
+  },
+  {
+    "revision": "9978b1e7a2b6b25d558f",
+    "url": "/static/js/reactPlayerFilePlayer.d4530511.chunk.js"
+  },
+  {
+    "revision": "2d34e08dc2c0eff88e38",
+    "url": "/static/js/reactPlayerMixcloud.5aa3fc8d.chunk.js"
+  },
+  {
+    "revision": "88965dfbdbda4f49ec66",
+    "url": "/static/js/reactPlayerPreview.43db1b71.chunk.js"
+  },
+  {
+    "revision": "49c01aef5bae1194373c",
+    "url": "/static/js/reactPlayerSoundCloud.03c61b97.chunk.js"
+  },
+  {
+    "revision": "d280c80281d18720a036",
+    "url": "/static/js/reactPlayerStreamable.875f9c3c.chunk.js"
+  },
+  {
+    "revision": "6b6701049e945869784b",
+    "url": "/static/js/reactPlayerTwitch.8db6386e.chunk.js"
+  },
+  {
+    "revision": "10f16075f8d7fc2f3e05",
+    "url": "/static/js/reactPlayerVidyard.997987d2.chunk.js"
+  },
+  {
+    "revision": "e11435f6dfe4bcea1b8a",
+    "url": "/static/js/reactPlayerVimeo.7e4c8498.chunk.js"
+  },
+  {
+    "revision": "0e7ea07e354835a75e6b",
+    "url": "/static/js/reactPlayerWistia.74a4586d.chunk.js"
+  },
+  {
+    "revision": "3935022eb02fdb01fe35",
+    "url": "/static/js/reactPlayerYouTube.47eacdb4.chunk.js"
+  },
+  {
+    "revision": "0509b0269ac82e0e8b6d",
+    "url": "/static/js/runtime-main.966a06f0.js"
+  },
+  {
+    "revision": "9e2c0f787c447f84b14b19ee615c7fe2",
+    "url": "/static/media/favicon_white.9e2c0f78.png"
+  },
+  {
+    "revision": "76bd4143c2a1bf09fa00e48b9a8e6eeb",
+    "url": "/static/media/paxos.76bd4143.jpg"
+  }
+]);
